a5aaa6f1b92513b662137d10ac9c9d659617d469
Kasbolat Kumakhov <uhfath@gmail.com>
2022-08-30 12:39:44 +0300

    Add project files.

---
+++ eFarmaResultProcessor/Options.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFarmaResultProcessor
+{
+	internal class Options
+	{
+		public string OutputSuffix { get; set; }
+		public string SourceNodes { get; set; }
+		public string ResultNodes { get; set; }
+		public string DocumentIdNode { get; set; }
+		public string StatusNode { get; set; }
+		public string RemoveStatuses { get; set; }
+		public bool AutoCloseOnError { get; set; }
+
+		public HashSet<string> RemoveStatusesList => System.Text.Json.JsonSerializer.Deserialize<string[]>(RemoveStatuses).ToHashSet();
+	}
+}
---
+++ eFarmaResultProcessor/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace eFarmaResultProcessor
+{
+	internal class Program
+	{
+		private static ISet<string> GetFiles(IEnumerable<string> sources) =>
+			sources
+				.Where(s => Directory.Exists(s))
+				.SelectMany(s => Directory.EnumerateFiles(s, "*.*", SearchOption.AllDirectories))
+				.Concat(sources
+					.Where(s => File.Exists(s)))
+				.Where(s => string.Equals(Path.GetExtension(s), ".xml", StringComparison.OrdinalIgnoreCase))
+				.ToHashSet()
+			;
+
+		private static int ProcessReturn(int code, bool autoClose)
+		{
+			if (!autoClose)
+			{
+				Console.WriteLine("Нажмите ENTER для выхода");
+				Console.Read();
+			}
+
+			return code;
+		}
+
+		private static int Main(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+				.AddIniFile("config.ini")
+				.AddCommandLine(args)
+				.Build()
+			;
+
+			var options = configuration.GetSection("Main").Get<Options>();
+
+			if (!args.Any())
+			{
+				Console.Error.WriteLine("Не указаны файлы/папки для обработки");
+				return ProcessReturn(1, options.AutoCloseOnError);
+			}
+
+			var sources = args
+				.Where(a => File.Exists(a) || Directory.Exists(a))
+			;
+
+			var files = GetFiles(sources);
+			if (!files.Any())
+			{
+				Console.Error.WriteLine("Указанные файлы недоступны или не в том формате");
+				return ProcessReturn(2, options.AutoCloseOnError);
+			}
+
+			return 0;
+		}
+	}
+}
